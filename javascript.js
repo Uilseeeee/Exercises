0054f526c570b1082d81c903d5bfbdbf248a5588
Uilsee <uilseeu437@gmail.com>
2024-12-06 12:05:14 +0800

    test

--- javascript.js
+++ javascript.js
@@ -50,18 +50,7 @@
 // }
 // let avarage = sum/studentAges.length
 // console. log(avarage);
-let balance = [1000, 2000, 2000, 2500, 5000, 4000]
-let i = 0
-while (i < balance.length) {
-    balance[i] *= 1.15
-    i++;
-}
-console.log(balance);
-for (let i = 0; i < x.length; i++) {
-    for (let j = 0; j < x[0].length; j++) {
-        console.log(x[i][j]);
-    }
-}
+
 
 // let balance = [1000, 2000, 2000, 2500, 5000, 4000]
 // let i = 0
@@ -76,3 +65,16 @@ for (let i = 0; i < x.length; i++) {
 //     }
 // }
 
+"ARRAY"
+let balance = [1000, 2000, 2000, 2500, 5000, 4000]
+let i = 0
+while (i < balance.length) {
+    balance[i] *= 1.15
+    i++;
+}
+console.log(balance);
+for (let i = 0; i < x.length; i++) {
+    for (let j = 0; j < x[0].length; j++) {
+        console.log(x[i][j]);
+    }
+}
