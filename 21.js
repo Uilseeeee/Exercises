3f685e215c5c337c9a4b6802e8f5575b3c312de4
Uilsee <uilseeu437@gmail.com>
2024-12-06 11:55:10 +0800

    thinking

---
+++ 21.js
@@ -0,0 +1,7 @@
+let a=82
+let b=77
+let c=88
+let p=66
+if (a>80){
+    
+}
--- javascript.js
+++ javascript.js
@@ -1,35 +1,60 @@
-// variable 
-let a = 5
-let b = 6
-let c = 7
-let p =(a+b+c)
+// // variable 
+// let a = 5
+// let b = 6
+// let c = 7
+// let p =(a+b+c)
 
-// example-2 
-let d = 5 
-let e = 7
-f =(d+e)
-// example-3 
-let a2=5
-let p1=(a2*a2*a2)
-let s1=(a2*a2*6)
-//  example-4
-let a3=6
-let b3=4
-let p2=(a3*b3)
-let s2=(2*(a3+b3))
-// example-5 
-let x=1
-let y=3*x-5
-// example-6
-let x2=-2
-let y2=4*(x2*x2)-3*x2+5
-// example-7 
-let m=476
-let l=476%10
-// example-8
-let x3=423
-let p3=423%100
-let p4=(p3-(p3%10))/10
-// example-9
+// // example-2 
+// let d = 5 
+// let e = 7
+// f =(d+e)
+// // example-3 
+// let a2=5
+// let p1=(a2*a2*a2)
+// let s1=(a2*a2*6)
+// //  example-4
+// let a3=6
+// let b3=4
+// let p2=(a3*b3)
+// let s2=(2*(a3+b3))
+// // example-5 
+// let x=1
+// let y=3*x-5
+// // example-6
+// let x2=-2
+// let y2=4*(x2*x2)-3*x2+5
+// // example-7 
+// let m=476
+// let l=476%10
+// // example-8
+// let x3=423
+// let p3=423%100
+// let p4=(p3-(p3%10))/10
+// // example-9
+// example ioi
+// for (let i = 0 ; i < 3 ; i++){
+//     console.log("ioi");
 
+// }
+// let studentnames=["micheal","michelle","Jizelle"]
+// let fruits=new 
+// array["apple","pineapple","liir"]
+"ARRAY"
+// let studentAges = [10, 20, 15, 19, 18,19
+// , 11]
+// let i = 0;
+// let sum = 0;
+// while (i < studentAges.length) {
+// sum+=studentAges [i]
+// i++;
+// }
+// let avarage = sum/studentAges.length
+// console. log(avarage);
+let balance = [1000 , 2000 ,2000,2500,5000,4000]
+let i=0 
+while (i<balance.length){
+    balance[i] *=1.15
+    i++;
+}
+console.log(balance);
 
