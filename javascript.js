c1ee99b5c34eeb9c2a75b3544ee36b01e661a9b9
Uilsee <uilseeu437@gmail.com>
2024-12-12 11:34:27 +0800

    Javascript,html

--- javascript.js
+++ javascript.js
@@ -1,99 +1,19 @@
-// // variable 
-// let a = 5
-// let b = 6
-// let c = 7
-// let p =(a+b+c)
+const username = document.querySelector('#username').value
+const password = document.querySelector('#password').value
 
-// // example-2 
-// let d = 5 
-// let e = 7
-// f =(d+e)
-// // example-3 
-// let a2=5
-// let p1=(a2*a2*a2)
-// let s1=(a2*a2*6)
-// //  example-4
-// let a3=6
-// let b3=4
-// let p2=(a3*b3)
-// let s2=(2*(a3+b3))
-// // example-5 
-// let x=1
-// let y=3*x-5
-// // example-6
-// let x2=-2
-// let y2=4*(x2*x2)-3*x2+5
-// // example-7 
-// let m=476
-// let l=476%10
-// // example-8
-// let x3=423
-// let p3=423%100
-// let p4=(p3-(p3%10))/10
-// // example-9
-// example ioi
-// for (let i = 0 ; i < 3 ; i++){
-//     console.log("ioi");
 
-// }
-// let studentnames=["micheal","michelle","Jizelle"]
-// let fruits=new 
-// array["apple","pineapple","liir"]
-// "ARRAY"
+const containerElement = document.createElement('div')
+const welcomeElement = document.createElement('p')
+const usernameElement = document.createElement('p')
+const passwordElement = document.createElement('p')
 
-// let balance = [1000, 2000, 2000, 2500, 5000, 4000]
-// let i = 0
-// while (i < balance.length) {
-//     balance[i] *= 1.15
-//     i++;
-// }
-// console.log(balance);
-// for (let i = 0; i < x.length; i++) {
-//     for (let j = 0; j < x[0].length; j++) {
-//         console.log(x[i][j]);
-//     }
-// }
-// console.log(balance);
-// for (let i = 0; i < x.length; i++) {
-//     for (let j = 0; j < x[0].length; j++) {
-//         console.log(x[i][j]);
-//     } 
-// function primenumber() {
-//     for(let i=2 ;i<=n/2 ;i++){
-//         if(n%1==0)return"no"
-//     }
-//     return"yes"
-// }
-// console.log(primenumber);
-// Array[1,3,4,5]{
-//     for (let index = 0; index < array.length; index++) {
-//         const element = array[index];
-        
-//     }
-// }
+welcomeElement.innerText = "Welcome to our web site, hope you enjoy it"
+usernameElement.innerText = "Your username is" + username;
+passwordElement.innerText = "Yourpassword is" + password;
 
-// let fruits = ["Apple","Melon","Mango","Orange"]
-// let size = fruits.length;
-// console.log(fruits);
-// function system(wh) {
-    
-// }
-// let n = 5;
+containerElement.appendChild(welcomeElement);
+containerElement.appendChild(username);
+containerElement.appendChild(passwordElement);
 
-// while (n < 3) {
-//   n++;
-// }
-
-// console.log(n);
-// // Expected output: 3
-// let a=2
-// let b=3
-// function problem(params) {
-// problem=a+b
-// }
-// console.log(params);
-// let l = 5
-// while (n < 3){
-//     n++;
-// }
-// /
+document.body.appendChild(containerElement)
+sssss
